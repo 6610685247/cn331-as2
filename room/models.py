db19e4566cf07db1fb2fcd4ae230fab9fe221612
Punnawat Namkum <6610685247@cn330>
2025-09-11 14:46:43 +0700

    add floor to db

--- room/models.py
+++ room/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 class Room(models.Model) :
     room_id = models.CharField(max_length=3, primary_key=True)
-    room_name = models.CharField(max_length=50)
-    cap = models.IntegerField()
+    room_name = models.CharField(max_length=50, null=True)
+    cap = models.IntegerField(null=True)
     bookable = models.BooleanField(default=True)
     status = models.BooleanField(default=False)
+    floor = models.CharField(max_length=10, blank=True, null=True)
 
     def get_status(self):
         return "On" if self.status else "Off"
