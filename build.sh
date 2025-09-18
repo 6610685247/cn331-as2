1d82923f13e298ab3736a15de004f93f6b6bd74e
Kittichet Sintaosathapon <6610685106@student.tu.ac.th>
2025-09-18 21:58:32 +0700

    change superuser in build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --username admin --email "admin@gmail.com" --password 1234 --noinput || true
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin','admin@gmail.com','1234')"
