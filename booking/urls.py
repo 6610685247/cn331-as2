d976d302f2399230a4eda4720aa32bab0463d464
Kittichet Sintaosathapon <6610685106@student.tu.ac.th>
2025-09-18 21:32:07 +0700

    edit base on warnig

--- booking/urls.py
+++ booking/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('/booking/room<int:room_number>/', views.booking_page, name='booking_page'),
+    path('booking/room<int:room_number>/', views.booking_page, name='booking_page'),
     path('my_booking/', views.my_booking, name='my_booking'),
     path("cancel/<int:booking_id>/", views.cancel_booking, name="cancel_booking"),
 ]
