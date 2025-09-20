309c2811eab8796950736fd9fa6faa1b6f8611ac
Kittichet Sintaosathapon <6610685106@student.tu.ac.th>
2025-09-20 14:00:52 +0700

    edit build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
+
 pip install --upgrade pip
+
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
+
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin','admin@gmail.com','1234')"
+
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_EMAIL=admin@gmail.com
+export DJANGO_SUPERUSER_PASSWORD=1234
+
+python manage.py createsuperuser --noinput || true
