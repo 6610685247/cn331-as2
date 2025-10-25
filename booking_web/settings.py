2dee584ea8ed7f09b6fb5d9a3e417390c9bb8113
Kittichet Sintaosathapon <6610685106@student.tu.ac.th>
2025-10-26 00:21:12 +0700

    update db

--- booking_web/settings.py
+++ booking_web/settings.py
@@ -5,8 +5,9 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "change-this-secret-key")
-DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = ["*"]
+DEBUG = os.environ.get("DEBUG", "True") == "True"
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
